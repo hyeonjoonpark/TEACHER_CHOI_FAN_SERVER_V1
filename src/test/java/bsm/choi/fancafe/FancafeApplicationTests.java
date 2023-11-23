03d3823304c49921d4b2e40bb0f1c28ebc444147
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-24 06:44:02 +0900

    SET: 프로젝트 세팅, 시큐리티 세팅

---
+++ src/test/java/bsm/choi/fancafe/FancafeApplicationTests.java
@@ -0,0 +1,13 @@
+package bsm.choi.fancafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
