4f9335fc10cc320b579ad2c415f376aa581e13b4
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-24 11:16:08 +0900

    FEAT: security adminA

--- src/main/java/bsm/choi/fancafe/global/config/SecurityConfig.java
+++ src/main/java/bsm/choi/fancafe/global/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package bsm.choi.fancafe.global.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -15,10 +18,13 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(
                         (auth) -> auth
-                                .requestMatchers("/api/login").permitAll()
-                                .requestMatchers("/admin").hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                ).formLogin(Customizer.withDefaults());
+                                .requestMatchers("/").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().permitAll()
+                )
+                .formLogin(withDefaults()); // LoginForm 사용
+
+
 
         return http.build();
     }
