76465f81770bec9b0888242c4a97ffb0a574dbff
hyeonjoonpark <pjjoon1379@gmail.com>
2024-02-26 22:31:51 +0900

    fix: cors

--- src/main/java/bsm/choi/fancafe/global/config/WebConfig.java
+++ src/main/java/bsm/choi/fancafe/global/config/WebConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*");
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedMethods("*")
+      .allowCredentials(false)
+      .allowedOrigins("*");
+  }
 }
