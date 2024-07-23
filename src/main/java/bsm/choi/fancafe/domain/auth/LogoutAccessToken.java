1a4c9a70bd1debd58c946ffa89f5c8186728f684
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-23 18:43:24 +0900

    FEAT: RefreshToken Entity

--- src/main/java/bsm/choi/fancafe/domain/auth/LogoutAccessToken.java
+++ src/main/java/bsm/choi/fancafe/domain/auth/LogoutAccessToken.java
@@ -11,9 +11,9 @@ import org.springframework.data.redis.core.TimeToLive;
 
 @Entity
 @Getter
-@RedisHash("refreshToken")
+@RedisHash("logoutAccessToken")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class RefreshToken {
+public class LogoutAccessToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,22 +21,24 @@ public class RefreshToken {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
     private String token;
+
     @TimeToLive
     private Long expiresIn;
 
     @Builder
-    public RefreshToken(User user, String token, Long expiresIn) {
+    public LogoutAccessToken(User user, String token, Long expiresIn) {
         this.user = user;
         this.token = token;
         this.expiresIn = expiresIn;
     }
 
-    public static RefreshToken from(User user, String token, Long expiresIn) {
-        return RefreshToken.builder()
+    public static LogoutAccessToken from(User user, String token, Long expiresIn) {
+        return LogoutAccessToken.builder()
                 .user(user)
                 .token(token)
-                .expiresIn(expiresIn / 1000L)
+                .expiresIn(expiresIn)
                 .build();
     }
 }
