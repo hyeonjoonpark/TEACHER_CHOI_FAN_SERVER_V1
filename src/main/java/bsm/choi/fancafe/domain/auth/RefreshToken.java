75444609fc53d64640a4955766f5571b53226e0b
hyeonjoonpark <pjjoon1379@gmail.com>
2024-08-03 18:42:59 +0900

    FIX: refreshToken

--- src/main/java/bsm/choi/fancafe/domain/auth/RefreshToken.java
+++ src/main/java/bsm/choi/fancafe/domain/auth/RefreshToken.java
@@ -1,6 +1,5 @@
 package bsm.choi.fancafe.domain.auth;
 
-import bsm.choi.fancafe.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,35 +8,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
-@Entity
 @Getter
-@RedisHash("refreshToken")
+@RedisHash(value = "refreshToken", timeToLive = 2592000L)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken {
     @Id
-    private String id;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
     private String token;
+
     @TimeToLive
     private Long expiresIn;
 
     @Builder
-    public RefreshToken(String id, User user, String token, Long expiresIn) {
-        this.id = id;
-        this.user = user;
+    public RefreshToken(String token, Long expiresIn) {
         this.token = token;
         this.expiresIn = expiresIn;
-    }
-
-    public static RefreshToken from(String id, User user, String token, Long expiresIn) {
-        return RefreshToken.builder()
-                .id(id)
-                .user(user)
-                .token(token)
-                .expiresIn(expiresIn / 1000L)
-                .build();
     }
 }
